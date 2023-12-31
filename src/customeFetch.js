bcd92caf38c591ff6610a2fdea37db8fb95340a7
raja <memonabdulrehman250@gmail.com>
2023-12-31 19:35:11 +0500

    changed server to local

--- src/customeFetch.js
+++ src/customeFetch.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const customFetch = axios.create({
-    baseURL: "http://192.168.18.27:3000/"
+    baseURL: "192.168.18.27:3000/"
     // baseURL: "http://localhost:3000/"
 })
